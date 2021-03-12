9aee2b97af350921777ed1fef7ccb996a9242d12
YUZUFX\user <yustar.shooter@gmail.com>
2021-03-12 08:43:22 +0700

    load recycler to mainMenuFragment

--- app/src/main/java/com/yuzu/themoviedb/view/fragment/MainMenuFragment.kt
+++ app/src/main/java/com/yuzu/themoviedb/view/fragment/MainMenuFragment.kt
@@ -8,9 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.SimpleItemAnimator
 import com.yuzu.themoviedb.databinding.FragmentMainMenuBinding
 import com.yuzu.themoviedb.utils.BOTTOM_SHEET_DIALOG
+import com.yuzu.themoviedb.view.adapter.MovieAdapter
 import com.yuzu.themoviedb.viewmodel.MainMenuViewModel
+import java.util.*
 
 /**
  * Created by Yustar Pramudana on 10/03/2021
@@ -20,6 +23,8 @@ class MainMenuFragment: Fragment() {
     private val LOG_TAG = "User"
     private lateinit var binding: FragmentMainMenuBinding
     private lateinit var viewModel: MainMenuViewModel
+
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -37,7 +42,36 @@ class MainMenuFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         categoryOnClick()
+        initAdapter()
+        initState()
+    }
+
+    private fun initAdapter() {
+        movieAdapter = MovieAdapter(viewModel) { viewModel.retry() }
+        binding.recyclerView.adapter = movieAdapter
+        viewModel.moviePagedList.observe(viewLifecycleOwner, {
+            try {
+                Log.e("Paging ", "PageAll" + it.size)
+                try {
+                    //to prevent animation recyclerview when change the list
+                    binding.recyclerView.itemAnimator = null
+                    (Objects.requireNonNull(binding.recyclerView.getItemAnimator()) as SimpleItemAnimator).supportsChangeAnimations = false
+                } catch (e: Exception) {
+                }
+                movieAdapter.submitList(it)
+            } catch (e: Exception) {
+            }
+        })
+    }
+
+    private fun initState() {
+        viewModel.getState().observe(viewLifecycleOwner, { state ->
+            viewModel.recyclerViewVisibility(binding, state, movieAdapter)
+            //footerBinding.progressBar.visibility = if (viewModel.listIsEmpty() && state == State.LOADING) View.VISIBLE else View.GONE
+            //footerBinding.txtError.visibility = if (viewModel.listIsEmpty() && state == State.ERROR) View.VISIBLE else View.GONE
+        })
     }
 
     private fun categoryOnClick() {
--- app/src/main/java/com/yuzu/themoviedb/viewmodel/MainMenuViewModel.kt
+++ app/src/main/java/com/yuzu/themoviedb/viewmodel/MainMenuViewModel.kt
@@ -1,9 +1,23 @@
 package com.yuzu.themoviedb.viewmodel
 
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.yuzu.themoviedb.TheMovieDBApplication
+import com.yuzu.themoviedb.databinding.FragmentMainMenuBinding
+import com.yuzu.themoviedb.model.State
+import com.yuzu.themoviedb.model.data.MovieData
+import com.yuzu.themoviedb.model.datasource.PopularDataSource
+import com.yuzu.themoviedb.model.datasource.PopularDataSourceFactory
+import com.yuzu.themoviedb.model.repository.MovieRepository
+import com.yuzu.themoviedb.view.adapter.MovieAdapter
+import io.reactivex.disposables.CompositeDisposable
 
 /**
  * Created by Yustar Pramudana on 10/03/2021
@@ -13,10 +27,61 @@ class MainMenuViewModel: ViewModel() {
     private val LOG_TAG = "MainMenu"
     var loading: MutableLiveData<Boolean> = MutableLiveData(false)
 
+    private val compositeDisposable = CompositeDisposable()
+    private val movieRepository: MovieRepository
+    private val popularDataSourceFactory: PopularDataSourceFactory
+
+    var moviePagedList: LiveData<PagedList<MovieData>>
+
+    private val pageSize = 5
+
+    init {
+        val appComponent = TheMovieDBApplication.instance.getAppComponent()
+        movieRepository = appComponent.movieRepository()
+
+        popularDataSourceFactory = PopularDataSourceFactory(movieRepository, compositeDisposable)
+        val config = PagedList.Config.Builder().setPageSize(pageSize).setInitialLoadSizeHint(pageSize).setEnablePlaceholders(false).build()
+        moviePagedList = LivePagedListBuilder(popularDataSourceFactory, config).build()
+    }
+
     fun checkPrevFragment(prev: Fragment?, fragmentTransaction: FragmentTransaction) {
         if (prev != null) {
             fragmentTransaction.remove(prev)
         }
         fragmentTransaction.addToBackStack(null)
+    }
+
+    fun retry() {
+        popularDataSourceFactory.popularDataSourceLiveData.value?.retry()
+    }
+
+    fun getState(): LiveData<State> = Transformations.switchMap(
+        popularDataSourceFactory.popularDataSourceLiveData,
+        PopularDataSource::state
+    )
+
+    private fun listIsEmpty(): Boolean {
+        return moviePagedList.value?.isEmpty() ?: true
+    }
+
+    fun recyclerViewVisibility(binding: FragmentMainMenuBinding, state: State, movieAdapter: MovieAdapter) {
+        if (listIsEmpty() && state == State.LOADING) {
+            loading.value = true
+            binding.recyclerView.visibility = View.GONE
+            //binding.txtError.visibility = View.GONE
+
+        } else if (listIsEmpty() && state == State.ERROR) {
+            loading.value = false
+            //binding.txtError.visibility = View.VISIBLE
+
+        } else {
+            loading.value = false
+            binding.recyclerView.visibility = View.VISIBLE
+            //binding.txtError.visibility = View.GONE
+        }
+
+        if (!listIsEmpty()) {
+            movieAdapter.setState(state)
+        }
     }
 }
